f5e1f3c42b8bd037ace23af63f360dc47977550c
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-10-15 00:25:18 +0200

    reporting script completed now.

--- sub-cleaner.py
+++ sub-cleaner.py
@@ -6,6 +6,11 @@ from pathlib import Path
 if __name__ == '__main__':
     try:
         main(Path(__file__).absolute().parent)
+        print("Sub-cleaner completed successfully.")
+        exit()
     except KeyboardInterrupt:
-        print("Interrupted")
+        print("Sub-cleaner was Interrupted.")
+        exit()
+    except PermissionError as e:
+        print("Sub-cleaner ran into an permission error. Permission denied to: \"" + e.filename + "\"")
         exit()
