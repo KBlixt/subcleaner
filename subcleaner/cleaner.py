c4e7eb9bf19748eb0ab5e9c0ee02933fee22203b
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-10-26 15:23:01 +0200

    cleaner also fixes overlaps

--- subcleaner/cleaner.py
+++ subcleaner/cleaner.py
@@ -1,6 +1,7 @@
 from .subtitle import Subtitle
 from .sub_block import SubBlock
 from re import findall, IGNORECASE
+from datetime import timedelta
 
 
 def clean(subtitle: Subtitle, regex_list) -> list:
@@ -18,6 +19,8 @@ def clean(subtitle: Subtitle, regex_list) -> list:
 
     for block in clean_delete_blocks:
         subtitle.remove_block(block)
+
+    _fix_overlap(subtitle)
 
     return clean_delete_blocks
 
@@ -77,3 +80,22 @@ def _remove_ads_end(subtitle: Subtitle) -> list:
         if block.regex_matches > 0:
             delete_blocks.append(block)
     return delete_blocks
+
+
+def _fix_overlap(subtitle: Subtitle) -> None:
+    if len(subtitle.blocks) < 2:
+        return
+
+    margin: timedelta = timedelta(seconds=0.0417)
+    previous_block: SubBlock = subtitle.blocks[0]
+    for block in subtitle.blocks[1:]:
+        block: SubBlock
+        stop_time: timedelta = previous_block.stop_time + margin
+        start_time: timedelta = block.start_time - margin
+        overlap: timedelta = stop_time - start_time
+        if overlap.days >= 0 and overlap.microseconds > 3000:
+            content_ratio = len(block.content) / (len(block.content) + len(previous_block.content))
+            block.start_time += content_ratio * overlap
+            previous_block.stop_time += (content_ratio-1) * overlap
+        previous_block = block
+    return
