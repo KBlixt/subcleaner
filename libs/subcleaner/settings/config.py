8173538984946c2702f203589e9c4a0f7e310ba1
Karl.a.Blixt <karl.a.blixt@gmail.com>
2023-01-17 23:26:47 +0100

    config refactoring and cleaning deprecated stuff.

--- libs/subcleaner/settings/config.py
+++ libs/subcleaner/settings/config.py
@@ -34,18 +34,8 @@ cfg.read(str(config_file), encoding="UTF-8")
 use_default_regex = cfg['SETTINGS'].getboolean("use_defaults", True)
 
 sections = cfg.sections()
-if "REGEX" in sections and "PURGE_REGEX" not in sections:
-    # for backwards-compatibility:
-    cfg.add_section("PURGE_REGEX")
-    for key, value in cfg.items("REGEX"):
-        cfg.set("PURGE_REGEX", key, value)
-    cfg.remove_section("REGEX")
 
-if cfg.has_section("PURGE_REGEX") or cfg.has_section("WARNING_REGEX"):
-    logger.warning("Config file is out of date. Converting the config file to follow latest config-layout will enable "
-                   "more granular ad-detection and warnings.")
-
-log_dir = Path(cfg["SETTINGS"].get("log_dir", "log/"))
+log_dir = Path(cfg["SETTINGS"].get("log_dir", "logs/"))
 if not log_dir.is_absolute():
     log_dir = home_dir.joinpath(log_dir)
 if not log_dir.exists():
@@ -54,7 +44,7 @@ if not log_dir.is_dir():
     raise ValueError(f"log directory: {log_dir} is not a directory")
 log_file = log_dir.joinpath("subcleaner.log")
 
-relative_base = Path(cfg['SETTINGS'].get("relative_path_base", ""))
+relative_base = Path(cfg['SETTINGS'].get("relative_path_base", "."))
 if not relative_base.is_absolute():
     relative_base = Path.cwd().joinpath(relative_base)
 relative_base = relative_base.resolve()
@@ -66,7 +56,7 @@ if default_language in ["blank", "Blank", "", "empty", "Empty"]:
     default_language = None
 if default_language:
     if not languages.is_language(default_language):
-        logger.error("Config error: default language code in must a valid ISO:639 language. Exiting")
+        logger.error("Config error: default language code must be a valid ISO:639 language. Exiting")
         exit(1)
 
 use_english_on_all = cfg['SETTINGS'].getboolean("use_english_on_all", True)
