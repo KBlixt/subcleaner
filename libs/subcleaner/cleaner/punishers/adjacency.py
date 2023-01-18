618b9e1426a86a8ece0b72610d00f5977502e749
Karl.a.Blixt <karl.a.blixt@gmail.com>
2023-01-18 23:49:21 +0100

    improved regex, reduced adjacency sensitivity.

--- libs/subcleaner/cleaner/punishers/adjacency.py
+++ libs/subcleaner/cleaner/punishers/adjacency.py
@@ -5,33 +5,34 @@ from libs.subcleaner.subtitle import Subtitle
 
 
 def punish_ad_adjacency(subtitle: Subtitle) -> None:
-    blocks_to_punish: Set[SubBlock] = set()
+    nearby_blocks: Set[SubBlock] = set()
     for index in range(0, len(subtitle.blocks)):
         block = subtitle.blocks[index]
         if index < 3:
-            blocks_to_punish.add(block)
+            nearby_blocks.add(block)
             block.hints.append("close_to_start")
             continue
         if index > len(subtitle.blocks) - 4:
-            blocks_to_punish.add(block)
+            nearby_blocks.add(block)
             block.hints.append("close_to_end")
             continue
         for compare_block in subtitle.blocks[max(0, index - 15): min(index + 16, len(subtitle.blocks))]:
             if compare_block.regex_matches >= 3 and compare_block != block:
-                blocks_to_punish.add(block)
+                nearby_blocks.add(block)
                 block.hints.append("nearby_ad")
                 break
-    for block in blocks_to_punish:
-        block.regex_matches += 1
-    blocks_to_punish.clear()
 
+    adjacent_blocks: Set[SubBlock] = set()
     for index in range(0, len(subtitle.blocks)):
         block = subtitle.blocks[index]
         for compare_block in subtitle.blocks[max(0, index - 1): min(index + 2, len(subtitle.blocks))]:
             if compare_block.regex_matches >= 2 and compare_block != block:
-                blocks_to_punish.add(block)
+                adjacent_blocks.add(block)
                 break
-    for block in blocks_to_punish:
+
+    for block in nearby_blocks:
+        block.regex_matches += 1
+
+    for block in adjacent_blocks:
         block.regex_matches += 1
         block.hints.append("adjacent_ad")
-    blocks_to_punish.clear()
