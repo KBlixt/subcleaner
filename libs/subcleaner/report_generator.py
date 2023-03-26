387ac10c52bac5ff1fe5a869b25c5e36416adca1
Karl Blixt <32000265+KBlixt@users.noreply.github.com>
2023-03-26 15:02:04 +0200

    Fixed old -d command suggestion.

--- libs/subcleaner/report_generator.py
+++ libs/subcleaner/report_generator.py
@@ -22,7 +22,7 @@ def generate_report(subtitle: Subtitle) -> str:
         _add(_warning_card(subtitle.warning_blocks), " " * 40)
         _add("")
         _add("To delete all remaining warnings run:")
-        _add(f"python3 '{config.script_file}' '{subtitle.short_path}' -d {' '.join(subtitle.get_warning_indexes())}")
+        _add(f"python3 '{config.script_file}' '{subtitle.short_path}' --destroy {' '.join(subtitle.get_warning_indexes())}")
 
     return _report[1:]
 
