261ef5979e3661b57ff764dc9fa7083cda79969f
Karl.a.Blixt <karl.a.blixt@gmail.com>
2023-09-17 12:09:05 +0200

    reworked regex to compile the regex.

--- libs/subcleaner/cleaner/punishers/regex.py
+++ libs/subcleaner/cleaner/punishers/regex.py
@@ -12,15 +12,17 @@ def punish_regex_matches(subtitle: Subtitle) -> None:
         _run_regex_on_block(block, regex_lists.get_warning_regex(subtitle.language), 1)
 
 
-def _run_regex_on_block(block: SubBlock, regex_list: List[Tuple[str, str]], punishment: int) -> None:
+def _run_regex_on_block(block: SubBlock, regex_list: List[Tuple[str, re.Pattern]], punishment: int) -> None:
     clean_content = " ".join(block.content.replace("-\n", "-").split())
     for regex in regex_list:
         try:
-            result = re.findall("(" + regex[1] + ")", clean_content, flags=re.IGNORECASE | re.UNICODE)
+            result = re.findall(regex[1], clean_content)
             if result and isinstance(result[0], str):
+                result = [r.lower() for r in result]
                 result = set(result)
             else:
-                result = set([t[0] for t in result])
+                result = set([t[0].lower() for t in result])
+
         except re.error as e:
             raise ValueError(f"regex {regex[0]} is miss configured: {e.msg}")
         if result:
